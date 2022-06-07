c1b5862ef46f12bfe51256f85f7451c32d27d82f
Francisco Crende <franciscocrende@epet12smandes.edu.ar>
2022-06-07 18:38:20 -0300

    añado cambios

---
+++ src/functions/main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow, ipcMain } = require ('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 600,
+        minWidth: 600,
+        minHeight: 480,
+        frame:false,
+        webPreferences:{
+            nodeIntegration:true,
+            contextIsolation:false
+        }
+    })
+
+    win.loadFile("principal.html")
+    ipcMain.on('minimizeApp', () => {
+        win.minimize()
+    })
+
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
