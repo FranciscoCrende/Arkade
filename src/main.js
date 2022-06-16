cb05772b69b5625fa319ebefacd2277d12b80fd7
Pedro Nashe <pedronashe@gmail.com>
2022-06-16 19:05:24 -0300

    cambio fondo

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ function createWindow() {
     })
    
     //win.loadFile('src/index.html');
-    win.loadFile('src/view/log-in.html');
+    win.loadFile('src/view/principal.html');
     win.webContents.openDevTools()
 
     ipcMain.on('minimizeApp', () => {
