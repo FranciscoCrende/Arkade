f1a1f81086dfee304a1b7c3af97c041563d55c66
Francisco Crende <franciscocrende@epet12smandes.edu.ar>
2022-08-19 12:11:35 -0300

    Creacion de carpeta view en app react

---
+++ arcade-app/src/view/Login.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+
+function Login() {
+    return (
+        <form className="formulario">
+
+            <h1 id="registerlog_text">INICIAR SESION</h1>
+            <div className="contenedor">
+
+
+
+                <div className="input-contenedor">
+                    <i className="fas fa-envelope icon"></i>
+                    <input type="text" placeholder="Correo Electronico"/>
+
+                </div>
+
+                <div className="input-contenedor">
+                    <i className="fas fa-key icon"></i>
+                    <input type="password" placeholder="Contraseña"/>
+
+                </div>
+                <input type="submit" value="Login" className="button"/>
+                    <p>Al  registrarte ,  aceptas  nuestras  Condiciones  de  uso  y  Políticas  de  privacidad.</p>
+                    <p>¿No tienes una cuenta? <a className="link" href="register.html">Registrate </a></p>
+            </div>
+        </form>
+    )
+    
+
+}
+export default Login;
