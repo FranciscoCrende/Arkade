693706c66278c6996d54b8b4cf803a6f61c7513f
Lautaro Huechante <lautarohuechante@epet12smandes.edu.ar>
2022-09-16 10:28:18 -0300

    comitde prueba

---
+++ arcade-app/src/view/store.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Store() {
+    return 
+                (
+              
+    <div className="container">
+        <div className="back">
+            <a href="../view/principal.html">&#60 BACK</a>
+        </div>
+        <div id="logo">
+            ARKADE
+        </div>
+        <div className="container-tienda">
+            <li className="pociones"><img className="img1" src="../img/manzana.png" alt=""/>Pocion 1 <button>+</button></li>
+            <li className="pociones"><img className="img2" src="../img/pocion1.png" alt=""/>Pocion 2 <button>+</button></li>
+            <li className="pociones"><img className="img3" src="../img/pocion2.png" alt=""/>Pocion 3 <button>+</button></li>
+            <li className="pociones"><img className="img4" src="../img/manzanad.png" alt=""/>Pocion 4 <button>+</button></li>
+            <li className="pociones"><img className="img5" src="../img/pocion3.png" alt=""/>Pocion 5 <button>+</button></li>
+        </div>
+    </div>
+            )
+}
+
+export default Store;
