c7e2a9dcb4a74d20553806eb36cca022638e99ab
Lautaro Huechante <lautarohuechante@epet12smandes.edu.ar>
2022-09-16 10:40:01 -0300

    score/stoew

--- arcade-app/src/view/score.tsx
+++ arcade-app/src/view/score.tsx
@@ -2,7 +2,37 @@ import React from "react";
 
 function Score() {
     return(
-       
+                <><div className="money">
+            <img src="../img/coin.png" alt="" />1244
+        </div><div id="logo">
+                <h1>ARKADE</h1>
+            </div><div className="hiscore">
+                <h2>hi-score: 12000</h2> <h2>player: jugador 1</h2>
+
+            </div><div className="container-score">
+                <table className="table-score">
+                    <tr className="fila1">
+                        <th></th>
+                        <th>SCORE</th>
+                        <th>PLAYER</th>
+                    </tr>
+                    <tr className="fila2">
+                        <td>N°1</td>
+                        <td>12000</td>
+                        <td>Jugador 1</td>
+                    </tr>
+                    <tr className="fila3">
+                        <td>N°2</td>
+                        <td>10000</td>
+                        <td>Jugador 2</td>
+                    </tr>
+                    <tr className="fila4">
+                        <td>N°3</td>
+                        <td>8000</td>
+                        <td>Jugador 3</td>
+                    </tr>
+                </table>
+            </div></>
     )
 
  
