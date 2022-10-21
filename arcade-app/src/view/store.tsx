041328d1bc0c61ed4cdf0f0a298123d452257fd8
Pedro Nashe <pedronashe@gmail.com>
2022-10-21 12:54:34 -0300

    cambiando menu para item con descripcion

--- arcade-app/src/view/store.tsx
+++ arcade-app/src/view/store.tsx
@@ -5,18 +5,22 @@ import { Link, useNavigate } from 'react-router-dom';
 import Menu from '../models/Menu';
 import Menus from '../componenst/Menus';
 import "../styles/tienda.css"
+
+
+
 function Store() {
     const [menuSeleccionado, setMenuSeleccionado] = useState(0);
     const navigate = useNavigate()
-
+    const items: { menu: Menu, description: string }[] = [
+        { menu: { title: "Pocion 1", function: () => navigate("/Principal"), icon: "pocion1.png" }, description: "Esta pocion otorgara a la nave un x2 de volocidad de disparo" },
+        { menu: { title: "Pocion 2", function: () => navigate("/Principal"), icon: "pocion2.png" }, description: "Esta pocion otorgara al player resistancia a los tres primeros golpes"  },
+        { menu: { title: "Pocion 3", function: () => navigate("/Principal"), icon: "pocion3.png" }, description: "Esta pocion otorgara al player "  },
+        { menu: { title: "Pocion 4", function: () => navigate("/Principal"), icon: "manzana.png" }, description: "Esta pocion otorgara al de velocidad"  },
+        { menu: { title: "Pocion 5", function: () => navigate("/Principal"), icon: "manzanad.png" }, description: "Esta pocion otorgara al player un x2 de velocidad"  },
+    ]
     let menus: Menu[] = [
-
         { title: "PRESS ENTER", function: () => navigate("/Principal") },
-        { title: "Pocion 1", function: () => navigate("/Principal"), icon: "pocion1.png" },
-        { title: "Pocion 2", function: () => navigate("/Principal"), icon: "pocion2.png" },
-        { title: "Pocion 3", function: () => navigate("/Principal"), icon: "pocion3.png" },
-        { title: "Pocion 4", function: () => navigate("/Principal"), icon: "manzana.png" },
-        { title: "Pocion 5", function: () => navigate("/Principal"), icon: "manzanad.png" },
+        ...items.map(i => i.menu)
     ]
     return (
 
@@ -44,7 +48,7 @@ function Store() {
                 <h1>Pocion 1</h1>
                 <img className="pocion1" src="http://localhost:3000/img/pocion1.png" />
                 <p>Multiplica la velocidad del player x4</p>
-                    </div>
+            </div>
         </div>
     )
 }
